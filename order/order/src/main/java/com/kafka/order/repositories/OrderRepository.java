1dfab9a472c37617d52c1b80eb48ef2bcbfcddc4
jatin393 <57959964+jatin393@users.noreply.github.com>
2024-02-07 14:27:58 +0530

    Delete order/order directory

--- order/order/src/main/java/com/kafka/order/repositories/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kafka.order.repositories;
-
-import com.kafka.order.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
